799212dd0dd699f1a6ff0c33364f6b9d5b28ab5c
Joe Naples <jnaples@zorustech.com>
2023-01-30 17:52:56 -0500

    added ability to update data in db

--- src/supabase.js
+++ src/supabase.js
@@ -1,8 +1,8 @@
-import { createClient } from '@supabase/supabase-js';
+import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = 'https://ljrslkkigkfhssueaqua.supabase.co';
+const supabaseUrl = "https://ljrslkkigkfhssueaqua.supabase.co";
 const supabaseKey =
-  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImxqcnNsa2tpZ2tmaHNzdWVhcXVhIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NzQ3NDM0OTIsImV4cCI6MTk5MDMxOTQ5Mn0.LwqnS2yRPS7v7EzRcj62340x31V2UzFYuCt_WJ5MGmY';
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImxqcnNsa2tpZ2tmaHNzdWVhcXVhIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NzQ3NDM0OTIsImV4cCI6MTk5MDMxOTQ5Mn0.LwqnS2yRPS7v7EzRcj62340x31V2UzFYuCt_WJ5MGmY";
 const supabase = createClient(supabaseUrl, supabaseKey);
 
 export default supabase;
